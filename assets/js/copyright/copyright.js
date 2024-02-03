168f40ac19a4e5a38bf3be5f9cd178dccdf65a34
Muhaimin Shihab <talk.shihab@gmail.com>
2024-02-03 21:18:19 +0600

    remove: unused scss files

--- assets/js/copyright/copyright.js
+++
@@ -1,6 +0,0 @@
-const copyright = document.querySelector('.currentyear');
-
-const currentDate = new Date();
-const getCurrentYear = currentDate.getFullYear();
-
-copyright.innerHTML = getCurrentYear;
