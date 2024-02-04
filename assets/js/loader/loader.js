fd69fc79f3a6aced89f30e5f70b8e44b2d2a9d86
Muhaimin Shihab <talk.shihab@gmail.com>
2024-02-04 17:55:11 +0600

    remove: preloader

--- assets/js/loader/loader.js
+++
@@ -1,7 +0,0 @@
-const preloader = document.querySelector('#preloader');
-
-function showHidePreloader() {
-  preloader.classList.add('d-none');
-}
-
-window.addEventListener('load', showHidePreloader);
